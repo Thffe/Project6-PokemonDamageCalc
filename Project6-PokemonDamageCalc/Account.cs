7a559d45d6f8a05aa7fba4ffed1c0b101d3eb4d7
Bean <145775095+Padole05@users.noreply.github.com>
2026-02-05 13:47:31 -0500

    add 2 classes

---
+++ Project6-PokemonDamageCalc/Account.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection.Metadata;
+
+namespace Project6_PokemonDamageCalc
+{
+    public class Account
+    {
+        int accountID;
+        char username;
+        Blob pfp;
+
+        public Account(int accountID, char username, Blob pfp)
+        {
+            this.accountID=accountID;
+            this.username=username;
+            this.pfp=pfp;
+        }
+
+        public void createAccount(char username)
+        {
+            this.username=username;
+        }
+
+        public void deleteAccount(char username)
+        {
+
+        }
+
+        public void login(char username)
+        {
+        }
+
+        public void logout(char username)
+        {
+        }
+
+        public void uploadPic(Blob pfp)
+        {
+
+        }
+
+        
+
+    }
+
+}
---
+++ Project6-PokemonDamageCalc/Pokelist.cs
@@ -0,0 +1,23 @@
+﻿namespace Project6_PokemonDamageCalc
+{
+    public class Pokelist
+    {
+        int teamID;
+        int accountID;
+        Pokemon poke1;
+        Pokemon poke2;
+        Pokemon poke3;
+        Pokemon poke4;
+        Pokemon poke5;
+        Pokemon poke6;
+        public DamageCalc dmgcalc;
+
+        public Pokelist(int teamID, int accountID)
+        {
+            this.teamID=teamID;
+            this.accountID=accountID;
+        }
+
+        
+    }
+}
